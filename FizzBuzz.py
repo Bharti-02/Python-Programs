f5b28f72b30b56923fac3d8276972b99329991df
Bharti <160905596+Bharti-02@users.noreply.github.com>
2025-06-23 20:58:20 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,8 @@
 # multiple of 3 print Fizz , multiple of 5 print Buzz, multilple of 3,5 print fizzbuzz
 
+#user input
 n=int(input("Enter number:"))
+
 
 for i in range(1,n+1):
     if i%3==0 and i%5==0:
